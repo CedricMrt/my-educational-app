f5a861c7696683aad52c452c41c81836f5d1abef
CedricMrt <cjyiphone@gmail.com>
2024-12-26 18:32:24 +0100

    fix bug

--- src/app/utils/StudentGuard.tsx
+++ src/app/utils/StudentGuard.tsx
@@ -2,24 +2,31 @@
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../lib/firebaseConfig";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const StudentGuard = ({ children }: { children: React.ReactNode }) => {
   const [user] = useAuthState(auth);
   const router = useRouter();
+  const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
-    if (!user) {
-      const student = sessionStorage.getItem("student");
-      if (student) {
-        router.push("/student/dashboard");
-      } else {
-        router.push("/");
+    setIsClient(true);
+  }, []);
+
+  useEffect(() => {
+    if (isClient) {
+      if (!user) {
+        const student = sessionStorage.getItem("student");
+        if (student) {
+          router.push("/student/dashboard");
+        } else {
+          router.push("/");
+        }
       }
     }
-  }, [user, router]);
+  }, [user, router, isClient]);
 
-  if (!user && !sessionStorage.getItem("student")) {
+  if (!user && isClient && !sessionStorage.getItem("student")) {
     return null;
   }
 
