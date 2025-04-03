800211ae84877a99716ca1787fcd6365218496dd
CedricMrt <cjyiphone@gmail.com>
2025-04-03 19:03:06 +0200

    perf: :zap: remove schoolContext

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { SchoolProvider } from "./utils/SchoolContext";
 import { Toaster } from "react-hot-toast";
 
 export const metadata: Metadata = {
@@ -15,16 +14,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='fr'>
-      <SchoolProvider>
-        <body
-          className={` bg-[url('/img/Hogwarts_Background.webp')] bg-cover bg-center bg-no-repeat`}
-        >
-          <div className='bg-[#0000001c] h-[92.3vh] landscape:h-[87.6vh]'>
-            <Toaster position='top-center' />
-            {children}
-          </div>
-        </body>
-      </SchoolProvider>
+      <body
+        className={` bg-[url('/img/Hogwarts_Background.webp')] bg-cover bg-center bg-no-repeat`}
+      >
+        <div className='bg-[#0000001c] h-[92.3vh] landscape:h-[87.6vh]'>
+          <Toaster position='top-center' />
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
